3a512edf7d14535581c3e70f8fcd26c11db0378e
robotnik1337 <flamingninja61@gmail.com>
2026-08-18 12:25:12 -0400

    fix: change typo in ModBlocks after sequoia forest merge

--- src/main/java/net/Bankgo/DistantDreams/block/ModBlocks.java
+++ src/main/java/net/Bankgo/DistantDreams/block/ModBlocks.java
@@ -423,7 +423,7 @@ public class ModBlocks {
             () -> new FertileSoilBlock(setIdProperty("fertile_soil", BlockBehaviour.Properties.ofFullCopy(Blocks.GRASS_BLOCK))));
 
     public static final RegistryObject<FarmBlock> FERTILE_PLOT = registerBlock("fertile_plot",
-            () -> new ModFarmBlock(setIdProperty("fertile_plot", BlockBehaviour.Properties.ofFullCopy(Blocks.FARMLAND))));
+            () -> new FertilePlotBlock(setIdProperty("fertile_plot", BlockBehaviour.Properties.ofFullCopy(Blocks.FARMLAND))));
 
 
     // ===== END FERTILE ===== //
